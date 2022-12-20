5e90c35b91f6f750223c5d759ee6c096d4b04aa6
prashant giri <pgiri570@gmail.com>
2022-12-20 15:19:00 +0530

    adding words to backend

--- src/features/word/words.route.js
+++ src/features/word/words.route.js
@@ -3,9 +3,32 @@ const express = require("express");
 const app = express.Router();
 
 app.get("/", async (req, res) => {
-  var words = ["Rock", "Paper", "Scissors"];
+  var words = [
+    "Rock",
+    "Paper",
+    "Scissors",
+    "Absence",
+    "Academic",
+    "Boundary",
+    "Borrow",
+    "Characteristic",
+    "Dialogue",
+    "Embrace",
+    "Foreign",
+    "Generate",
+    "Highlight",
+    "Incorporate",
+    "Justify",
+    "Obligation",
+    "Portray",
+    "Qualify",
+    "Refused",
+    "Tactic",
+    "Undergo",
+    "Violation",
+  ];
   var word = words[Math.floor(Math.random() * words.length)];
-  res.send(word)
+  res.send(word);
 });
 
 module.exports = app;
